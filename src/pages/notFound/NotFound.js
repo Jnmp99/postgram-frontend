2a4f16d7aac500f34e054e92d1ae6997f90a4118
Jnmp99 <113456459+Jnmp99@users.noreply.github.com>
2022-11-06 20:05:10 -0600

    Delete src directory

--- src/pages/notFound/NotFound.js
+++
@@ -1,5 +0,0 @@
-const NotFound = () => {
-  return <div style={{ color: "white" }}>Not found</div>;
-};
-
-export default NotFound;
--- src/resources/profile/BackIcon.js
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-
-const BackIcon = (props) => (
-  <svg xmlns="http://www.w3.org/2000/svg" height={48} width={48} {...props}>
-    <path d="M24 40 8 24 24 8l2.1 2.1-12.4 12.4H40v3H13.7l12.4 12.4Z" />
-  </svg>
-);
-
-export default BackIcon;
